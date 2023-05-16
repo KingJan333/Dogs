7042ca08adc2a8f40e8b2236a1ef1d18a8375e4a
VitaliyVT <ToropchinVV@yandex.ru>
2023-05-16 18:43:30 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+  import { ref } from "vue";
+  import axios from "axios";
+
+  import Loader from './components/LoaderComponent/Loader.vue'
+
+const isLoaded = ref(true);
+  const result = ref({});
+
+  axios({
+    method: "GET",
+    url: "https://dog.ceo/api/breeds/image/random",
+  })
+    .then((resp) => {
+      isLoaded.value =false
+      result.value = resp.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  const reloadImg = () => {
+    axios({
+      method: "GET",
+      url: "https://dog.ceo/api/breeds/image/random",
+    })
+      .then((resp) => {
+        result.value = resp.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+</script>
+
+<template>
+<Loader v-if="isLoaded"/>
+  <div class="wrapp-dogs" v-if="!isLoaded">
+    <img class="dog-img" :src="result.message" alt="dog" />
+    <button class="dog-btn" @click="reloadImg">Новая картинка</button>
+  </div>
+</template>
+
+<style scoped>
+  .wrapp-dogs {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .dog-img {
+    width: 80%;
+    border-radius: 10px;
+  }
+
+  .dog-btn {
+    cursor: pointer;
+    border: none;
+    background: rgb(63, 94, 251);
+    background: linear-gradient(
+      90deg,
+      rgba(63, 94, 251, 1) 0%,
+      rgba(252, 70, 107, 1) 100%
+    );
+    color: white;
+    width: 80%;
+    margin-top: 30px;
+    padding: 10px 0;
+    border-radius: 10px;
+  }
+</style>
---
+++ src/components/LoaderComponent/Loader.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <span class="loader"></span>
+</template>
+
+<style scoped>
+.loader {
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  position: relative;
+  animation: rotate 1s linear infinite;
+  display: block;
+}
+.loader::before , .loader::after {
+  content: "";
+  box-sizing: border-box;
+  position: absolute;
+  inset: 0px;
+  border-radius: 50%;
+  border: 5px solid rgb(63, 94, 251);
+  animation: prixClipFix 2s linear infinite ;
+      background: linear-gradient(
+      90deg,
+      rgba(63, 94, 251, 1) 0%,
+      rgba(252, 70, 107, 1) 100%
+    );
+}
+.loader::after{
+  transform: rotate3d(90, 90, 0, 180deg );
+  border-color: rgb(63, 94, 251);
+}
+
+@keyframes rotate {
+  0%   {transform: rotate(0deg)}
+  100%   {transform: rotate(360deg)}
+}
+
+@keyframes prixClipFix {
+  0%   {clip-path:polygon(50% 50%,0 0,0 0,0 0,0 0,0 0)}
+  50%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 0,100% 0,100% 0)}
+  75%, 100%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,100% 100%,100% 100%)}
+}
+</style>
