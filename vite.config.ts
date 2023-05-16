cdf97da261782f9429cd8afb711ebc57959a7e13
VitaliyVT <ToropchinVV@yandex.ru>
2023-05-16 19:43:44 +0300

    Add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,41 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      outDir: "dist",
+      devOptions: {
+          enabled: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Dogs',
+        short_name: 'Dogs',
+        description: 'Собакены',
+        theme_color: 'black',
+        icons: [
+          {
+              src: '/dog-192.jpeg',
+              sizes: '192x192',
+              type: 'image/png'
+          },
+          {
+              src: '/dog-512.jpeg',
+              sizes: '512x512',
+              type: 'image/png'
+          }
+      ],
+      }
+    })
+  ],
   server: {
-    port: 1345
+    port: 1345,
+    host: true
   }
 })
